e6d48c64109b141630272a340083458190aa6773
fraalmei <104362305+fraalmei@users.noreply.github.com>
2023-07-15 16:50:28 +0200

    Revert "Merge branch 'main' of https://github.com/fraalmei/So_long"
    This reverts commit 9e78378c1b16cdb147ddae92a70fdd5dadba9218, reversing
    changes made to 6066a04938d603c6b6244f79d0720f1c3475fa2c.

--- libft/ft_intlen.c
+++ libft/ft_intlen.c
@@ -1,7 +1,6 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-<<<<<<<< HEAD:libft/ft_intlen.c
 /*   ft_intlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fraalmei <fraalmei@student.42.fr>          +#+  +:+       +#+        */
@@ -28,23 +27,3 @@ size_t	ft_intlen(int n)
 	}
 	return (i);
 }
-========
-/*   map.h                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fraalmei <fraalmei@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/10 15:45:27 by fraalmei          #+#    #+#             */
-/*   Updated: 2023/03/11 13:02:09 by fraalmei         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#ifndef MAP_H
-# define MAP_H
-
-# include <so_long.h>
-# include <window.h>
-
-
-
-#endif
->>>>>>>> 36f35b83b776079048e96fb7aaf44dec9629781a:include/map.h
